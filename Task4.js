79bb6cf09afade0f6f534148715c13b1e986b73e
ksm44 <81572748+ksm44@users.noreply.github.com>
2021-04-01 14:16:57 +0300

    Delete scrip4.js and edit Task 4

--- Task4.js
+++ Task4.js
@@ -1,55 +1,62 @@
 /* Напишите функцию, которая принимает массив с разными, иногда повторяющимися,
  числами, а возвращает массив состоящий только из чисел, которые повторялись.
  Порядок чисел в результирующем массиве должен соотвествовать порядку того как
- повторения чисел были найден. */
+ повторения чисел были найдены. */
 
- let array = [1, 3, 4, 7, 4, 2, 0, 28, 56, 56, 3, 0, 5, 5, 6, 7, 6];  
- let k = 1;
- let arr2 = [];
- function Repeat(arr) {
-   for (let i = 0; i < arr.length; i++) {
-       for (let j = i + 1; j <= i + 1 && i < arr.length - 1; j++) {
-         console.log(k);
+let array = [1, 3, 2, 2, 3, 0];  // Ожидается на выходе [2, 3]
 
-         if (arr[i] === arr[i+k]) {
-           arr2.push(arr[i]);
-         }
-       }
-     if (i === (arr.length - 1) && (k < arr.length - 1)) {
-       i = 0;
-       ++k;
-       console.log("it`s work!");
-       
-     }
-   }
-   arr2 = Unique(arr2);
-   return arr2;
- }
+function Repeat(arr) {
+let arr2 = [];
+let arr3 = [];
+let isEqual = false;
+arr2.push(arr[0]);
+
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      //console.log("arr2[j] = " + arr2[j] + " arr[i] = " + arr[i]);
+      if (arr[i] !== arr2[j]){
+        isEqual = false;
+      } 
+      if (arr[i] === arr2[j]) {
+        isEqual = true;
+        break;
+      }
+    }
+    if(isEqual === false){
+      arr2.push(arr[i]);
+    } else {
+      arr3.push(arr[i]);
+    }
+
+  }
+   return Unique(arr3);
+}
+
+
 
 
  function Unique(arr) {
   let isNotEqual = false;
-  let arr2 = [];
-  arr2.push(arr[0]);
+  let arr4 = [];
+  arr4.push(arr[0]);
  // alert(arr2);
   
   for (let i = 1; i < arr.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      arr2[j] !== arr[i] ? isNotEqual = true : isNotEqual = false;
+    for (let j = 0; j < arr4.length; j++) {
+      arr4[j] !== arr[i] ? isNotEqual = true : isNotEqual = false;
       if (!isNotEqual){
         break
       }      
     }    
     if (isNotEqual) {
-      arr2.push(arr[i]);
+      arr4.push(arr[i]);
       isNotEqual = false;
     }
   }
-  arr = arr2;
+  arr = arr4;
   return arr;
 }
 
 
  console.clear();
  console.log(Repeat(array));
- 
