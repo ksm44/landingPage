8d56853cbc1738496d480308a5dbedd661490333
ksm44 <81572748+ksm44@users.noreply.github.com>
2021-08-22 21:50:40 +0300

    Update Task18.js

--- Task18.js
+++ Task18.js
@@ -1,31 +1,58 @@
-/* Есть массив с некоторыми числами. Все числа равны, кроме одного. Попробуй найти!
-
-findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
-findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
-Гарантируется, что в массиве не менее 3-х чисел.
-
-Тесты содержат очень большие массивы, так что подумайте о производительности.  */
-
-let arr = [ 1, 1, 1, 2, 1, 1 ];
 
 
-function findUniq(arr) {
-  let i = 0;
-  let j = 1;
-  let k;
-  let x;
-  while (arr[i] === arr[j]) {
-    ++i;
-    ++j;
+function validSolution(board){
+  //TODO
+  let str = '';
+  //проверяем по горизонтали
+  for (let i = 0; i < board.length; i++) {
+   if (board[i].slice().sort().join('') !== '123456789') return false;
   }
 
-  k = j + 1;
-  if(arr[j] !== arr[k]){
-    x = arr[j];
-  } else x = arr[i];
-  return x;
+  //проверяем по вертикали
+  for (let j = 0; j < board.length; j++) {
+    for (let k = 0; k < board.length; k++) {
+      str += board[k][j];
+    }
+   if (str.split('').sort().join('') !== '123456789') return false;    
+    str = '';
+  }
+  str = '';
+  //проверяем блоки 3x3
+  let shiftX = 0;
+  let shiftZ = 0;
+
+  while (shiftX <= 6 && shiftZ < 6){
+    console.log("shiftX " + shiftX + " shiftZ " + shiftZ);
+  for (let z = shiftZ; z < shiftZ + 3; z++) {
+    for (let x = shiftX; x < shiftX + 3; x++) {
+      console.log(board[z][x]);
+      str += board[z][x];
+      
+    }
+  }
+  if (str.split('').sort().join('') !== '123456789') return false; 
+  str = '';
+  
+//  if(shiftX === 6 && shiftZ === 3){
+//
+//  } else {
+//    (shiftX === 6) ? (shiftZ += 3, shiftX = 0) : (shiftX += 3);
+//  }
+
+  }
+
+  return true;
 }
 
-
 console.clear();
-console.log(findUniq(arr));
+console.log(validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+])); // => true);
